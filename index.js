ab52eac26512ead0ffb836152cd37aeaa6809878
Csaba Todor <todorcs@gmail.com>
2017-01-01 16:49:21 +0200

    Set the path for static resources to 'public' folder

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var app = express();
 app.use(bodyParser.json()); // handles parsing json content
 app.use(bodyParser.urlencoded({extended: false})); // parse application/x-www-form-urlencoded
 
+// Set Path for Static Resources
+app.use(express.static(path.join(__dirname, "public")));
+
 // setup the route for the homepage using the get request
 app.get("/", function (req, res) {
     res.send("I am a response in the screen"); // .send method prints out message on the screen
