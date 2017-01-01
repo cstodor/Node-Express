8ec72916b651e785ddaac2329d11bf067965b1d8
Csaba Todor <todorcs@gmail.com>
2017-01-01 15:30:22 +0200

    Created index.js main entry file
    - imported body-parser for parsing requests
    - imported path for simplifying file patshs
    - setup port 3000 where the application will run
    - setup the route for the homepage ( "/" )
    - created favicon file

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser"); // handles parsing requests
+var path = require("path"); // simplifies file paths
+
+var app = express();
+
+// setup the route for the homepage using the get request
+})
+
+// setting up the port where the application will run
+app.listen(3000, function () {
+    console.log("Server Successfully started at Port 3000!");
+    console.log("I am a message in the command line!");
+})
