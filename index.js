4db7f4638f3e98d5d220e00687b2d39009aa6b00
Csaba Todor <todorcs@gmail.com>
2017-01-01 16:47:58 +0200

    Fixed home route

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ var path = require("path"); // simplifies file paths
 var app = express();
 
 // setup the route for the homepage using the get request
+app.get("/", function (req, res) {
+    res.send("I am a response in the screen"); // .send method prints out message on the screen
 })
 
 // setting up the port where the application will run
 app.listen(3000, function () {
-    console.log("Server Successfully started at Port 3000!");
+    console.log("Server Successfully Started at Port 3000!");
     console.log("I am a message in the command line!");
 })
