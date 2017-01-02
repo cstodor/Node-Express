068c05a51bc7bdc9b082e8ede8777d7425f378ef
Csaba Todor <todorcs@gmail.com>
2017-01-02 19:25:14 +0200

    Added View Engine Middleware with Custom Title

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ var path = require("path"); // simplifies file paths
 
 var app = express();
 
+// EJS Template Engine Middleware
+app.set('view engine', 'ejs'); // specify the view engine. ejs in our case
+app.set('views', path.join(__dirname, 'views')); // specify the folder that we want to use for our views
+
 // Body Parser Middleware / top-level generic (most simple)
 app.use(bodyParser.json()); // handles parsing json content
 app.use(bodyParser.urlencoded({extended: false})); // parse application/x-www-form-urlencoded
-
-// Set Path for Static Resources
+// Set Middleware for Public folder where Static Resources will be Stored
 app.use(express.static(path.join(__dirname, "public")));
 
 // setup the route for the homepage using the get request
 app.get("/", function (req, res) {
-    res.send("I am a response in the screen"); // .send method prints out message on the screen
+    // res.send("I am a response in the screen"); // .send method prints out message on the screen
+    res.render('index', {
+        title: 'Example Title'
+    });
 })
 
 // setting up the port where the application will run
